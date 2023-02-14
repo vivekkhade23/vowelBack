7137a8697df3610b9fff7b8f6168973fa1d83c74
vivekkhade23 <khadev881@gmail.com>
2023-02-15 02:24:18 +0530

    backend

--- src/model/cart.model.js
+++ src/model/cart.model.js
@@ -4,7 +4,7 @@ const cartSchema = new mongoose.Schema({
     title: { type: String, required: true },
     price: { type: Number, required: true },
     image: { type: String, required: true },
-    qty: { type: String, required: true }
+    qty: { type: Number, required: true }
 })
 
 const cartModal = mongoose.model("cart", cartSchema);
